56ee030d3d40c9a20bae239ce68e3153eb5d5ce2
general electrix <general.electrix@gmail.com>
2017-06-17 02:02:19 -0700

    ENH: progress on reactor types

--- console_server/src/lib.rs
+++ console_server/src/lib.rs
@@ -3,22 +3,25 @@
 
 extern crate event_loop;
 extern crate smallvec;
+extern crate serde;
 
 use event_loop::{EventLoop, Event};
 use std::error::Error;
 use std::sync::mpsc::{channel, Sender, Receiver};
 use std::time::Duration;
 use smallvec::SmallVec;
+use serde::{Serialize, Deserialize};
+
+struct LoadData;
 
 /// Outer command wrapper for the reactor, exposing administrative commands on top of the internal
 /// commands that the console itself provides.  Quitting the console, saving the show, and loading
 /// a different show are all considered top-level commands, as they require swapping out the state
 /// of the reactor.
-pub enum Command<C, S, L> {
-    /// Save the show, using a metadata type that will be injected by the console logic.
-    Save(S),
-    /// Load a different show, using a metadata type that will be injected by the console logic.
-    Load(L),
+pub enum Command<C> {
+    /// Load a different show, using a metadata type that is yet to be determined.
+    /// This may end up being a true path, or some kind of internal identifier.
+    Load(LoadData),
     /// Quit the console, cleanly closing down every running thread.
     Quit,
     /// A message to be passed into the console logic running in the reactor.
@@ -28,13 +31,11 @@ pub enum Command<C, S, L> {
 /// Outer command wrapper for a response from the reactor, exposing messages indicating
 /// that administrative actions have occurred, as well as passing on messages from the console
 /// logic running in the reactor.
-pub enum Response<R, S, L> {
-    /// A save action was successful.
-    Saved(S),
-    /// A save action failed.
-    SaveFailed(S),
-    /// A new show was loaded.
-    Loaded(L),
+pub enum Response<R, LE> {
+    /// A new show was loaded, with this name.
+    Loaded(String),
+    /// Show load failed.
+    LoadFailed(LE: Error),
     /// The console is going to quit.
     Quit,
     /// A response emanting from the console itself.
@@ -44,34 +45,25 @@ pub enum Response<R, S, L> {
 /// Small vector optimization for zero or 1 messages; console logic should use this type to return
 /// response messages.  May be removed if it turns out that consoles basically only ever emit one
 /// response message per command message.
-pub type ResponseMessages<T> = SmallVec<[T; 1]>;
-
-type RM<R, S, L> = ResponseMessages<Response<R, S, L>>;
+pub type Messages<T> = SmallVec<[T; 1]>;
 
 /// Console logic must implement this trait to be run in the Wiggles reactor.
 /// Note that none of these methods return Result; consoles are expected to be unconditionally
 /// stable as far as the reactor is concerned.  If they need to indicate expected/safe errors, that
 /// should be done in-band as part of the Response type.
-pub trait Console {
+pub trait Console<'de>: Serialize + Deserialize<'de> {
     /// The native command message type used by this console.
     type Command;
     /// The native response message type used by this console.
     type Response;
-    /// The metadata this console requires to save its state.
-    type Save;
-    /// The metadata this console requires to load a different show.
-    type Load;
     /// Render a show frame, potentially emitting messages.
-    fn render(&mut self) -> RM<Self::Response, Self::Save, Self::Load>;
+    fn render(&mut self) -> Messages<Self::Response>;
 
     /// Update the show state, potentially emitting messages.
-    fn update(&mut self, dt: Duration) -> RM<Self::Response, Self::Save, Self::Load>;
-
-    /// Autosave the show state.
-    fn autosave(&mut self);
+    fn update(&mut self, dt: Duration) -> Messages<Self::Response>;
 
     /// Handle a command, probably emitting messages.
-    fn handle_command(&mut self, command: Self::Command) -> RM<Self::Response, Self::Save, Self::Load>;
+    fn handle_command(&mut self, command: Self::Command) -> Messages<Self::Response>;
 }
 
 /// The heart of the console.
@@ -80,25 +72,29 @@ pub trait Console {
 /// renders the state of the show, and potentially keeps a store of autosaved data to ensure the
 /// console can recover from a crash without total disaster (even if you never remembered to hit
 /// save).
-pub struct Reactor<Cmd, Resp, Save, Load, C>
-    where C: Console
+pub struct Reactor<'de, C, LE>
+    where C: Console<'de>, LE: Error
         {
     console: C,
     event_source: EventLoop,
-    cmd_queue: Receiver<Command<Cmd, Save, Load>>,
-    resp_queue: Sender<Response<Resp, Save, Load>>,
+    cmd_queue: Receiver<Command<C::Command>>,
+    resp_queue: Sender<Response<C::Response, LE>>,
 }
 
-impl<Cmd, Resp, Save, Load, C> Reactor<Cmd, Resp, Save, Load, C>
-    where C: Console 
-        {
-    /// Run the console reactior.
-    pub fn run(&mut self) {
+// impl<Cmd, Resp, Save, Load, C> Reactor<Cmd, Resp, Save, Load, C>
+//     where C: Console 
+//         {
+//     /// Run the console reactor.
+//     pub fn run(&mut self) {
+//         loop {
+//             match self.event_source.next() {
+//                 Event::Idle(dt) => {
+//                     // we have dt until the next scheduled event.  use it to handle commands.
+//                     match self.
+//                 }
+//             }
+//         }
+//     }
 
-        loop {
-            match event_source.next() {
-
-            }
-        }
-    }
-}
+//     fn poll_command(&mut self) -> 
+// }
