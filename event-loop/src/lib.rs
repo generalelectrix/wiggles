fc2e1cbd126ffd4af92bec092dd75abb739572a8
general electrix <general.electrix@gmail.com>
2017-06-16 20:37:47 -0700

    ENH: some progress, but decided better to turn it inside out

--- event-loop/src/lib.rs
+++ event-loop/src/lib.rs
@@ -1,9 +1,8 @@
 //! An event loop for lighting controllers.
-//! Clients provide structs implementing the required traits and the event loop then drives the
+//! Clients provide a struct implementing the required trais and the event loop then drives the
 //! controller by calling the appropriate trait methods.
 //! Provides debug-level logging for tracing event flow.
-#![deny(missing_docs)]
-#![deny(missing_copy_implementations)]
+//! Uses channels for asynchronous command and response to control actions.
 
 extern crate log;
 extern crate smallvec;
@@ -12,22 +11,38 @@ use std::thread::sleep;
 use std::time::{Duration, Instant};
 use std::cmp;
 use std::error::Error;
+use std::sync::mpsc::{channel, Sender, Receiver};
+//use std::iter::Iterator;
 use smallvec::SmallVec;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 /// Event loop settings.
 pub struct Settings {
     /// Absolute fixed state update rate in fps.
-    updates_per_second: u64,
+    pub updates_per_second: u64,
     /// Maximum render rate in fps.
-    renders_per_second: u64,
+    pub renders_per_second: u64,
     /// Command the application to autosave at this interval (ms).
-    autosave_interval: u64,
+    /// Setting this to 0 disables autosave.
+    pub autosave_interval: u64,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Settings {
+            // update at 100 fps
+            updates_per_second: 100,
+            // DMX is limited to 50 fps max
+            renders_per_second: 50,
+            // By default do not autosave.
+            autosave_interval: 0,
+        }
+    }
 }
 
 /// Small vector optimization for output messages, ensuring the most common cases of 0 and 1
 /// output messages require no allocation.
-type OutputMessages<T> = SmallVec<[T; 1]>;
+type Responses<T> = SmallVec<[T; 1]>;
 
 /// Interface provided to the event loop by the show core.
 /// The fact that these methods all return events and not Results implies that the show's
@@ -35,27 +50,58 @@ type OutputMessages<T> = SmallVec<[T; 1]>;
 /// messages to be communicated back out to other layers of the stack.
 pub trait Actor {
     /// Used by the show to define the commands it accepts.
-    type InputMessage;
+    type Command;
     /// Returned by the show as a result of updating, rendering, or handling user input.
-    type OutputMessage;
+    type Response;
 
     /// Render the current state of the show.
     fn render(
         &mut self,
         frame_number: u64,
         time_since_update: Duration)
-        -> OutputMessages<Self::OutputMessage>;
+        -> Responses<Self::Response>;
 
     /// Update the current state of the show.
     fn update(
         &mut self,
         dt: Duration)
-        -> OutputMessages<Self::OutputMessage>;
+        -> Responses<Self::Response>;
 
     /// Handle a command message.
     fn handle_command(
         &mut self,
-        command: Self::InputMessage)
-        -> OutputMessages<Self::OutputMessage>;
+        command: Self::Command)
+        -> Responses<Self::Response>;
 }
 
+pub struct EventLoop<A: Actor> {
+    pub settings: Settings,
+    actor: A,
+    // Channel ends for sending and receiving messages.
+    cmd_send: Sender<A::Command>,
+    cmd_recv: Receiver<A::Command>,
+    resp_recv: Receiver<A::Response>,
+    resp_send: Sender<A::Response>,
+    
+}
+
+impl<A: Actor> EventLoop<A> {
+    /// Instantiate the event loop.
+    pub fn new(actor: A) -> Self {
+        let (cmd_send, cmd_recv) = channel();
+        let (resp_send, resp_recv) = channel();
+        EventLoop {
+            settings: Settings::default(),
+            actor: actor,
+            cmd_send: cmd_send,
+            cmd_recv: cmd_recv,
+            resp_recv: resp_recv,
+            resp_send: resp_send,
+        }
+    }
+
+    /// Get a clone of the command sender channel.
+    pub fn command_sender(&self) -> Sender<A::Command> {
+
+    }
+}
