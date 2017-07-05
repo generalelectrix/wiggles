80d43b69e3b4a8cac9d91444fdff8da3e8006b05
general electrix <general.electrix@gmail.com>
2017-07-04 22:12:39 -0700

    TST: test wiggle retrieval mechanism

--- dataflow/src/test/test_wiggle_network.rs
+++ dataflow/src/test/test_wiggle_network.rs
@@ -1,8 +1,9 @@
 use network::Network;
 use clocks::clock::{ClockValue, ClockProvider, ClockId};
+use wiggles_value::Unipolar;
 use wiggles::trial::{TestWiggle, CLASS as TEST_CLASS};
 use wiggles::new_wiggle;
-use wiggles::wiggle::WiggleNetwork;
+use wiggles::wiggle::{WiggleProvider, WiggleNetwork};
 use serde_json;
 
 struct TestClockProvider {}
@@ -17,9 +18,12 @@ impl ClockProvider for TestClockProvider {
 fn test_wiggle_network() {
     let mut network: WiggleNetwork = Network::new();
     let wiggle = new_wiggle(TEST_CLASS, "test wiggle").unwrap();
-    {
+    let wid = {
         let (wid, _) = network.add(wiggle);
-    }
+        wid
+    };
+
+    let val = network.get_value(wid, Unipolar(0.0), None, &TestClockProvider{});
 
     // check serialization/deserialization mechanism
     let ser_net = serde_json::to_string(&network).unwrap();
